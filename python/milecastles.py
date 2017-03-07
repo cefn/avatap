f39e6f5f5dd1e244d3f2ca8ae097d79a8b1f09c2
Cefn Hoile <github.com@cefn.com>
2017-03-07 09:08:27 +0000

    Moving towards initial console and testing API

--- python/milecastles.py
+++ python/milecastles.py
@@ -1,8 +1,8 @@
 story = None
 
-def loadStory(storyUid):
+def loadStory(storyIdString):
 	global story
-	story = __import__("stories." + storyUid).story
+	story = __import__("stories." + storyIdString).story
 
 '''
 Base class which treats all named arguments as attributes and 
@@ -10,12 +10,19 @@ all positional arguments as dicts containing named attributes
 '''
 class Item(object):
 	required=[]
-	def __init__(self, *args, **kwargs):
-		# populate attributes from positional and keyword args
+	defaults={}
+	
+	# populate attributes from positional and
+	def __init__(self, *args, **kwargs): 
 		for data in args[1:]: # exclude self
 			for key in data:
 				setattr(self, key, data[key])
 		for key in kwargs:
+			# check that uids are typed as Uid
+			if key.lower().endswith("uid"):
+				uidType = type(kwargs[key])
+				if not(uidType==Uid):
+					raise AssertionError("Attribute " + key + " with name ending 'uid' is actually of type " + uidType.__name__)
 			setattr(self, key, kwargs[key])
 
 		# raise error if any 'required' attributes are missing   
@@ -25,58 +32,169 @@ class Item(object):
 				missing.append(name)
 		if len(missing) > 0:
 			raise AssertionError(type(self).__name__ + " missing required attributes " + str(missing) )
+		
+		for key in type(self).defaults:
+			if not hasattr(self, key):
+				setattr(self, key, type(self).defaults[key])
+				
+class Container(Item):
 	
+	def _get_table(self, cls):
+		if not(hasattr(self, 'registry')):
+			self.registry = dict()
+		if not(cls.__name__ in self.registry):
+			 self.registry[cls.__name__]=dict()
+		return self.registry[cls.__name__]
+		
+	'''register any subclass of item to be looked up later by its id'''
+	def _register(self, cls, item):
+		table = self._get_table(cls)
+		if not(item.uid.idString in table):
+			table[item.uid.idString]=item
+		else:
+			raise AssertionError("Lookup table for " + cls.__name__ + " already contains item with uid" + item.uid.idString)
+
+	''''''
+	def _lookup(self, cls, uid):
+		if not(type(uid) == Uid):		
+			raise AssertionError("Attempted lookup with type" + type(uid) + "; should be Uid")	
+		else:
+			if hasattr(self, "registry"):
+				if cls.__name__ in self.registry:
+					table = self.registry[cls.__name__]
+					if uid.idString in table:
+						return table[uid.idString]
+			import pdb; pdb.set_trace()
+
+			raise AssertionError("'" + uid.idString + "' not in " + cls.__name__ + " lookup table")
+
+
+class Uid:
+	def __init__(self, idString):
+		self.idString = idString
+
+	"""Override the default Equals behavior"""
+	def __eq__(self, other):
+		if isinstance(other, self.__class__):
+			return self.idString == other.idString
+		return False
+
+	def __neq__(self, other):
+		return not self.__eq__(other)
+		
+	def __hash__(self):
+		return hash(type(self).__name__) ^ hash(self.idString)
+
+'''
+	A common superclass for items with ids. Accepts a string id argument
+	and silently replaces it with a typed Uid object which is required 
+	by other Uid oriented references, this supports implicit validation 
+	of data structures (e.g. encouraging you to refer to myPassage.uid in 
+	preference to the possibly "theuid")
+'''	
 class UidItem(Item):
 	required = Item.required + ["uid"]
-	pass
+	def __init__(self, *a, **k):
+		# intercept string uids, turn into Uid objects
+		uid = k["uid"]
+		if type(uid) == str:
+			k["uid"]=Uid(uid)
+		super().__init__(*a,**k)
 	
-class Story(UidItem):
+class Story(Container):
+	required = Container.required + ["startPassageUid"]
+	
 	def __init__(self, *args, **kwargs):
 		super().__init__(*args, **kwargs)
-		self.passages = dict()
-	
+			
 	def registerPassage(self, passage):
-		self.passages[passage.uid]=passage
+		return self._register(Passage, passage)
 	
 	def lookupPassage(self, passageUid):
-		return self.passages[passageUid]
+		return self._lookup(Passage, passageUid)
+
+	def registerBox(self, box):
+		return self._register(Box, box)
+	
+	def lookupBox(self, boxUid):
+		return self._lookup(Box, boxUid)
 
 class Card(UidItem):
 	required = UidItem.required + ["storyUid", "passageUid", "sack"]
 
+
 class Box(UidItem):
+	required = UidItem.required + ["label"]
 	def __init__(self, *args, **kwargs):
 		super().__init__(*args, **kwargs)
-		self.stories = dict()
-	
-	def registerStory(self, story):
-		self.stories[story.uid]=story
-	
-	def lookupStory(self, storyUid):
-		return self.stories[storyUid]
-	
-	def handle_card(self, card):
-		story = self.lookupStory(card.storyUid)
-		passage = story.lookupPassage(card.passageUid)
-		passage.handle_tap(self,card)
-				
+		self.story.registerBox(self)
+						
 class Passage(UidItem):
-	pass
+	def __init__(self, *args, **kwargs):
+		super().__init__(*args, **kwargs)
+		self.story.registerPassage(self)
+		
+	def handle_tap(self, engine, card):
+		raise AssertionError("Not yet implemented")
 
 class PagePassage(Passage):
-	required = Passage.required + ["showBoxUid", "page", "nextPassageUid"]
+	required = Passage.required + ["rightBoxUid", "rightText", "nextPassageUid"]
+	defaults = dict(Passage.defaults, **{
+		"wrongText":None
+	})
+	
+	def __init__(self, *a, **k):
+		super().__init__(*a,**k)
 					
-	def handle_tap(self, box, card):
-		if box.uid == self.showBoxUid:
-			story = box.lookupStory(card.storyUid)
-			nextPassage = story.lookupPassage(self.nextPassageUid)
-			if self.showBoxUid == nextPassage.showBoxUid:
-				box.render(self.page + "\n...tap to continue")
+	def handle_tap(self, engine, card):
+		box = engine.box
+		import pdb; pdb.set_trace()
+		if box.uid == self.rightBoxUid:
+			nextPassage = self.story.lookupPassage(self.nextPassageUid)
+			card.passageUid = nextPassage.uid
+			if self.rightBoxUid == nextPassage.rightBoxUid:
+				engine.render(self.rightText + "\n...tap to continue")
 			else:
-				box.render(self.page + "\n...now go to box" + self.nextUid)
+				engine.render(self.rightText + "\n...now go to " + self.story.lookupBox(nextPassage.rightBoxUid).label)				
 		else:
-			box.render(error="Wrong box. Go to box " + self.showUid)
+			if self.wrongText == None:
+				wrongText = "Please go to " + self.story.lookupBox(self.rightBoxUid).label + " to continue your adventure"
+			else:
+				wrongText = self.wrongText
+			engine.render(wrongText)
+		
+'''
+A passage which displays its text on tap, then requires you to tap 
+again to confirm navigation within a certain time. Failing to tap would 
+ConfirmationPassage(Passage):
+	def handle_tap():
+'''
+		
 
+'''
+# superceded by 'script' attribute which is evaluated in the context of a tap (e.g. "sack.money = sack.money + 5")
+# See also https://twine2.neocities.org/1.html for Twine reference features around variables and execution
+class ExecutePassage(PagePassage):
+	pass
+'''
+
+'''
+class WaitPassage(PagePassage):
+	pass
+'''
+
+class ChoicePassage(PagePassage):
+	pass
+	
+class ConditionalPassage(PagePassage):
+	pass
+
+
+# Condition render
+# and text rendering from sack
+# Condition routing
+# Test Verification of story
+# Rendering as graph
 
 class ConsoleBox(Box):
 	def render(self, text):
--- python/test.py
+++ python/test.py
@@ -1,72 +1,95 @@
 import unittest
 from milecastles import *
-from stories import tullie
+from engines import Engine
 
-def create_card():
-	return Card(
-		uid="abcdefgh",
-		storyUid = "teststory",
-		passageUid = "welcome",
-		sack = dict(
-			money=10,
-			health=10
-		)
-	)
-
-
-class MockBox(ConsoleBox):
+class MockEngine(Engine):
 	def __init__(self, *args, **kwargs):
 		super().__init__(*args, **kwargs)
 		self.render_calls = list()
 		
 	def render(self, *args, **kwargs):
 		self.render_calls.append((args, kwargs))
-		super().render(*args, **kwargs)
 
 class PageTest(unittest.TestCase):
 	
 	def __init__(self, *args, **kwargs):
 		super().__init__(*args, **kwargs)
 		self.num_boxes = 4
-	
+		
 	def setUp(self):
-		
+
 		# configure 'story' 
-		self.story = Story(
-			uid="teststory"
-		)
-		
-		self.passage = PagePassage(
-			uid="welcome",
-			page="Welcome to Milecastles. {instruction}",
-			showBoxUid='0',
-			nextPassageUid = 'welcome'
+		self.storyUid = Uid("teststory")
+		story = Story(
+			uid=self.storyUid
 		)
 
-		# add a circular passage
-		self.story.registerPassage(self.passage)
-
-		# configure 'boxes' containing the story
-		self.boxes = dict()
-		for boxPos in range(self.num_boxes):
-			boxUid = str(boxPos) # keyed by a str for json encoding
-			box = MockBox(uid=boxUid)
-			self.boxes[boxUid]=box
-			box.registerStory(self.story)
+		# configure boxes
+		self.boxes = [Box(story=story, uid=str(boxPos), label="The box called " + str(boxPos)) for boxPos in range(self.num_boxes)]
 		
-		# create an imaginary card
-		self.card = create_card()
+		# configure engines
+		self.engines = [MockEngine(box=box) for box in self.boxes]
+		for engine in self.engines:
+			engine.registerStory(story)
+
+		# create passage uids to wire up passages
+		self.landingUid = Uid("landing")
+		self.endingUid = Uid("ending")
+
+		# create a beginning passage which points to the end passage
+		PagePassage(story=story,
+			uid=self.landingUid,
+			rightBoxUid=self.engines[0].box.uid,
+			rightText="Welcome to Milecastles.",
+			nextPassageUid=self.endingUid
+		)
+
+		# create an ending passage for the story
+		PagePassage(story=story,
+			uid=self.endingUid,
+			rightBoxUid=self.engines[1].box.uid,
+			rightText="You have finished your adventure",
+			nextPassageUid=self.landingUid
+		)
+		
 
 	def tearDown(self):
-		del self.story, self.passage, self.boxes, self.card
+		del self.boxes, self.engines, self.storyUid, self.landingUid, self.endingUid
 			
 	def test_show_text(self):
-		showBox = self.boxes[self.passage.showBoxUid]		
-		showBox.handle_card(self.card)
-		assert len(showBox.render_calls) == 1
-		a, k = showBox.render_calls[0]
+		# create an imaginary card
+		card = Card(
+			uid="abcdefgh",
+			storyUid =self.storyUid,
+			passageUid = self.landingUid,
+			sack = dict(
+				money=10,
+				health=10
+			)
+		)
+				
+		# present the card to the relevant engine
+		engine = self.engines[0]
+		
+		#import pdb; pdb.set_trace()
+
+		engine.handle_card(card)
+
+		# check results
+		assert len(engine.render_calls) == 1
+		a, k = engine.render_calls[0]
 		print(a)
+		
+		assert card.passageUid == self.endingUid
 		return True
+
+'''
+class TullieStoryTest(unittest.TestCase):
+	def __init__(self, story, *args, **kwargs):
+		super().__init__(*args, **kwargs)
+		from stories import tullie
+		self.story = tullie.story	
+'''
 
 if __name__ == "__main__":
 	unittest.main()
