03fa8b3de6642f35f8df833a626c038a9a943154
Cefn Hoile <github.com@cefn.com>
2017-03-07 21:27:37 +0000

    Added debug object. Renamed function calls for consistency

--- python/engines/__init__.py
+++ python/engines/__init__.py
@@ -1,22 +1,43 @@
 from milecastles import Container, Story
 
+class Debug():
+	def report(report):
+		print(report)
+		
+	def debug(msg):
+		print("DEBUG:" + msg)
+
+	def info(msg):
+		print("INFO:" + msg)
+
+	def warn(msg):
+		print("WARN:" + msg)
+
+	def error(msg):
+		print("ERROR:" + msg)
+
+	def fatal(msg):
+		print("FATAL:" + msg)
+
 class Engine(Container):
-	required = Container.required + ["box"]
+	required = [n for n in Container.required if n!="uid"] + ["box"] # remove "uid"
 	
 	def __init__(self, *a, **k):
 		super().__init__(*a, **k)
+		self.debug = Debug()
 		
 	def registerStory(self, story):
 		return self._register(Story, story)
 	
 	def lookupStory(self, storyUid):
 		return self._lookup(Story, storyUid)
-		
-	def handle_card(self, card):
-		story = self.lookupStory(card.storyUid)
-		passage = story.lookupPassage(card.passageUid)
-		passage.handle_tap(self,card)
+				
+	def handleCard(self, card):
+		self.card = card
+		self.story = self.lookupStory(card.storyUid)
+		passage = self.story.lookupPassage(card.passageUid)
+		passage.handleTap(self)
 		
 	'''Should render some text, in whatever form required by the Engine'''
-	def render(self, text):
-		raise Error("Not yet implemented")
+	def renderText(self, text):
+		raise AssertionError("Not yet implemented")
