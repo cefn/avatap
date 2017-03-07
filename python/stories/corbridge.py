72b1d81fb93a6320913e69a973ef17a3fccaa3ce
Cefn Hoile <github.com@cefn.com>
2017-03-07 21:31:59 +0000

    Now using story as context manager to minimise boilerplate.

--- python/stories/corbridge.py
+++ python/stories/corbridge.py
@@ -1,16 +1,45 @@
 from milecastles import *
 
 # inspects the module to figure out the story name (e.g. corbridge)
-story = Story(__name__.split(".")[-1])
+storyUid = Uid(__name__.split(".")[-1])
 
-landingUid = Uid("landing")
-PagePassage(story=story,
-	uid=landingUid, 
-	page="Welcome to Milecastles at Corbridge!\nGet your registration card from the till and tap the symbol below to begin!"),
+landingPassageUid = Uid("landingIntro")
+
+# create story
+story = Story(
+	uid=storyUid,
+	startPassageUid = landingPassageUid
 )
 
-endingUid = Uid("ending")
-PagePassage(story=story,
-	uid=endingUid,
-	page="You have reached the end of your journey"
-)
+with story:
+	
+	# uids which need to be referenced
+	materialsPassageUid = Uid("materialIntro")
+	endingPassageUid = Uid("ending")
+
+	registrationBoxUid = Uid("registration")
+	workshopBoxUid = Uid("workshop")
+
+	# populate with boxes
+	Box( uid=registrationBoxUid, 	label="The Entrance, Box I")
+	Box( uid=workshopBoxUid, 		label="The Workshop, Box II")
+
+	# populate with passages
+	PagePassage(
+		uid=landingPassageUid,
+		nextPassageUid=materialsPassageUid,
+		rightBoxUid = registrationBoxUid,
+		rightBoxText =	"You are a stone mason working our of the military base at Corbridge." + 
+						"Flavinus, a respected young cavalryman has recently died and members\n" + 
+						"of the Burial Club have approached you to create a tombstone in his honour"
+	)
+
+	PagePassage(story=story,
+		uid=endingUid,
+		rightBoxUid=workshopBoxUid,
+		rightBoxText = 	"Your quest is to gather the materials, tools and inspiration to complete \n" + 
+						"the tombstone and honour the memory of Flavinus!" + 
+						"You have {money} for materials."
+	)
+
+
