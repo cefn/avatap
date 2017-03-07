a5b4168451f205ef65a3a9981e972b774bade9d9
Cefn Hoile <github.com@cefn.com>
2017-03-07 21:28:14 +0000

    First functioning test runs with console engine

--- python/engines/console.py
+++ python/engines/console.py
@@ -1,22 +1,24 @@
-from milecastles import Item,Box
+from milecastles import *
 from engines import Engine
 
-class ConsoleSiteEmulator(Item):
-
-	required = Item.required + ["story"]
+class ConsoleSiteEmulator(Container):
+	required = [n for n in Container.required if n!="uid"] + ["story"]
 
 	def __init__(self, *a, **k):
 		super().__init__(*a, **k)
 		
 		# get boxes from story registry, create+store an engine for each 
+		#import pdb; pdb.set_trace()
 		boxTable = self.story._get_table(Box)
 		self.engines = dict()		
 		for boxUid,box in boxTable.items():
-			self.engines[boxUid] = ConsoleEngine(box=box)
+			engine = ConsoleEngine(box=box)
+			engine.registerStory(self.story)
+			self.engines[boxUid] = engine
 			
 		# for each card, register it with the emulator
 		cardIds = ["a","b","c","d"]
-		for cardId in self.cardIds:
+		for cardId in cardIds:
 			card = Card(
 				uid=cardId,
 				storyUid =self.story.uid,
@@ -33,33 +35,33 @@ class ConsoleSiteEmulator(Item):
 	def lookupCard(self, cardUid):
 		self._lookup(Card, cardUid)
 		
-	def run():
+	def run(self):
 		while True:
 			command = input()
 			
 			# handle commands indentifying a card in the emulator
 			cardTable = self._get_table(Card) # use box lookup from story
 			if command in cardTable: 
-				self.currentCard = self.cards[command]
+				self.currentCard = cardTable[command]
 				print("Current card set to '" + command + "'")
 						
 			# handle commands identifying a box in the story
 			boxTable = self.story._get_table(Box) # use box lookup from story
 			if command in boxTable: 
 				if self.currentCard != None:
-					print("Tapping box'" + command + "' with card '" + self.currentCard.uid.idString + "'")
+					print("Tapping box '" + command + "' with card '" + self.currentCard.uid.idString + "'")
 					engine = self.engines[command]
-					engine.handle_card(self.currentCard)
+					engine.handleCard(self.currentCard)
 				else:
-					print("Cannot tap box. No card selected. Choose one of " + str(list(cards.keys())))
+					print("Cannot tap box. No card selected. Choose one of " + str(list(cardTable.keys())))
 	
 class ConsoleEngine(Engine):
 	
-	def render(self, text):
-		print("text")
+	def renderText(self, text):
+		print(text)
 		
 if __name__ == "__main__":
-	story = 
-	emulator = ConsoleSiteEmulator()	
-
-
+	from milecastles import loadStory
+	story = loadStory("example")
+	emulator = ConsoleSiteEmulator(story=story)
+	emulator.run()
