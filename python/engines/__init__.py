fba5bc91f483d587b1857b572ceeadd232200a40
Cefn Hoile <github.com@cefn.com>
2017-03-09 12:41:06 +0000

    Changed Passage to Node. Implemented expression and template rendering against a common set of names in an 'engine context'

--- python/engines/__init__.py
+++ python/engines/__init__.py
@@ -35,8 +35,49 @@ class Engine(Container):
 	def handleCard(self, card):
 		self.card = card
 		self.story = self.lookupStory(card.storyUid)
-		passage = self.story.lookupPassage(card.passageUid)
-		passage.handleTap(self)
+        self.setNodeUid(card.nodeUid)
+    
+    def setNodeUid(self, nodeUid):
+        try:
+            if self.node.uid != nodeUid:
+                self.node.deactivate(self)
+        except AttributeError:
+            
+        	node = self.story.lookupNode(card.nodeUid)
+        self.node = node
+        node.activate(self)
+        # if node not substituted, handle tap
+        if self.node == node:
+            node.handleTap(self)
+            
+    def getEngineContext():
+        return dict(
+            engine =    self, 
+            box =       self.box,
+            node =      self.node,
+            card =      self.card,
+            sack =      self.card.sack
+        )
+    
+    def evaluateExpression(self, expression):
+        return eval(self.expression, self.getEngineContext())
+    
+    def renderTemplate(self, templateString, templateResolver):
+        # prefix template with declaration of standard self, engine args
+        templateString = "{% args engine, box, node, card, sack %}\n" + templateString
+        template_in = io.StringIO(templateString)
+        template_out = io.StringIO()
+        c = Compiler(resolver, template_in, template_out)
+        try:
+            c.compile()
+            compiled = template_out.getvalue()
+            g = dict()
+            eval(compiled, g)
+            renderFun = g["render"]
+            self.renderText(renderFun(**self.getEngineContext()))
+        except Error as k:
+            import ipdb; ipdb.set_trace()
+            raise k
 		
 	'''Should render some text, in whatever form required by the Engine'''
 	def renderText(self, text):
