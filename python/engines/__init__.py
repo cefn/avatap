f70284502b039c751e9ad1657f01416053abac9a
Cefn Hoile <github.com@cefn.com>
2017-03-09 21:47:12 +0000

    Fixed indentation. Resolved implicit bug that engine#handleCard() didn't trigger node#handleTap(). Fixed bug where eval was used instead of exec. Resolved possibly incompatible string vs. stream semantics in renderTemplate

--- python/engines/__init__.py
+++ python/engines/__init__.py
@@ -1,57 +1,59 @@
+from agnostic import io
 from milecastles import Container, Story
 from boilerplate import Compiler
 
 class Debug():
-	def report(report):
-		print(report)
-		
-	def debug(msg):
-		print("DEBUG:" + msg)
+    def report(report):
+        print(report)
+        
+    def debug(msg):
+        print("DEBUG:" + msg)
 
-	def info(msg):
-		print("INFO:" + msg)
+    def info(msg):
+        print("INFO:" + msg)
 
-	def warn(msg):
-		print("WARN:" + msg)
+    def warn(msg):
+        print("WARN:" + msg)
 
-	def error(msg):
-		print("ERROR:" + msg)
+    def error(msg):
+        print("ERROR:" + msg)
 
-	def fatal(msg):
-		print("FATAL:" + msg)
+    def fatal(msg):
+        print("FATAL:" + msg)
 
 class Engine(Container):
-	required = [n for n in Container.required if n!="uid"] + ["box"] # remove "uid"
-	
-	def __init__(self, *a, **k):
-		super().__init__(*a, **k)
-		self.debug = Debug()
-		
-	def registerStory(self, story):
-		return self._register(Story, story)
-	
-	def lookupStory(self, storyUid):
-		return self._lookup(Story, storyUid)
-				
-	def handleCard(self, card):
-		self.card = card
-		self.story = self.lookupStory(card.storyUid)
+    required = [n for n in Container.required if n!="uid"] + ["box"] # remove "uid"
+    
+    def __init__(self, *a, **k):
+        super().__init__(*a, **k)
+        self.debug = Debug()
+        
+    def registerStory(self, story):
+        return self._register(Story, story)
+    
+    def lookupStory(self, storyUid):
+        return self._lookup(Story, storyUid)
+                
+    def handleCard(self, card):
+        self.card = card
+        self.story = self.lookupStory(card.storyUid)
         self.setNodeUid(card.nodeUid)
+        self.node.handleTap(self)
     
     def setNodeUid(self, nodeUid):
+        self.setNode(self.story.lookupNode(nodeUid))
+        
+    def setNode(self, node):
         try:
-            if self.node.uid != nodeUid:
+            if self.node != node:
                 self.node.deactivate(self)
         except AttributeError:
-            
-        	node = self.story.lookupNode(card.nodeUid)
+            pass
         self.node = node
-        node.activate(self)
-        # if node not substituted, handle tap
-        if self.node == node:
-            node.handleTap(self)
+        self.card.nodeUid = node.uid
+        self.node.activate(self)
             
-    def getEngineContext():
+    def getEngineContext(self):
         return dict(
             engine =    self, 
             box =       self.box,
@@ -65,21 +67,25 @@ class Engine(Container):
     
     def renderTemplate(self, templateString, templateResolver):
         # prefix template with declaration of standard self, engine args
-        templateString = "{% args engine, box, node, card, sack %}\n" + templateString
+        templateString = "{% args engine, box, node, card, sack %}" + templateString
         template_in = io.StringIO(templateString)
         template_out = io.StringIO()
-        c = Compiler(resolver, template_in, template_out)
+        c = Compiler(templateResolver, template_in, template_out)
         try:
             c.compile()
             compiled = template_out.getvalue()
             g = dict()
-            eval(compiled, g)
+            exec(compiled, g)
             renderFun = g["render"]
-            self.renderText(renderFun(**self.getEngineContext()))
-        except Error as k:
+            renderGen = renderFun(**self.getEngineContext())
+            renderOut = io.StringIO()
+            for chunk in renderGen:
+                renderOut.write(chunk)
+            self.renderText(renderOut.getvalue())
+        except Exception as k:
             import ipdb; ipdb.set_trace()
             raise k
-		
-	'''Should render some text, in whatever form required by the Engine'''
-	def renderText(self, text):
-		raise AssertionError("Not yet implemented")
+        
+    '''Should render some text, in whatever form required by the Engine'''
+    def renderText(self, text):
+        raise AssertionError("Not yet implemented")
