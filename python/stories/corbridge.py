ddc797714e344b9d090b8d5bd61350b82809c917
Cefn Hoile <github.com@cefn.com>
2017-03-09 14:14:29 +0000

    Drafting changes

--- python/stories/corbridge.py
+++ python/stories/corbridge.py
@@ -1,45 +1,53 @@
-from milecastles import *
+from milecastles import Uid, UidRegistry, Story, Box, LinearPassage
 
 # inspects the module to figure out the story name (e.g. corbridge)
-storyUid = Uid(__name__.split(".")[-1])
+storyName = __name__.split(".")[-1]
 
-landingPassageUid = Uid("landingIntro")
+storyUid = Uid(storyName)
+
+nodeUids = UidRegistry([
+        "landing",
+        "materialsIntro",
+        "workshopIntro",
+        "ending"
+])
+
+boxUids = UidRegistry([
+        "market",
+        "workshop",
+        "tomb",
+        "quarry"
+])
 
 # create story
 story = Story(
 	uid=storyUid,
-	startPassageUid = landingPassageUid
+	startPassageUid = nodeUids.landing
 )
 
 with story:
 	
-	# uids which need to be referenced
-	materialsPassageUid = Uid("materialIntro")
-	endingPassageUid = Uid("ending")
-
-	registrationBoxUid = Uid("registration")
-	workshopBoxUid = Uid("workshop")
-
 	# populate with boxes
-	Box( uid=registrationBoxUid, 	label="The Entrance, Box I")
-	Box( uid=workshopBoxUid, 		label="The Workshop, Box II")
+	Box( uid=boxUids.entrance,     label="The Entrance, Box I")
+	Box( uid=boxUids.workshop,     label="The Workshop, Box II")
 
 	# populate with passages
-	PagePassage(
-		uid=landingPassageUid,
-		nextPassageUid=materialsPassageUid,
-		rightBoxUid = registrationBoxUid,
-		rightBoxText =	"You are a stone mason working our of the military base at Corbridge." + 
-						"Flavinus, a respected young cavalryman has recently died and members\n" + 
+	LinearPassage(
+		uid=              nodeUids.landing,
+		visitBoxUid =     boxUids.market,
+		visitBoxText =	"You are a stone mason working our of the military base at Corbridge.\n" + 
+    						"Flavinus, a respected young cavalryman has recently died and members\n" + 
 						"of the Burial Club have approached you to create a tombstone in his honour"
+		nextNodeUid=   nodeUids.materials,
 	)
 
-	PagePassage(story=story,
-		uid=endingUid,
-		rightBoxUid=workshopBoxUid,
-		rightBoxText = 	"Your quest is to gather the materials, tools and inspiration to complete \n" + 
+	LinearPassage(
+		uid=              nodeUids.materialsIntro,
+		visitBoxUid =     boxUids.market,
+		visitBoxText = 	"Your quest is to gather the materials, tools and inspiration to complete \n" + 
 						"the tombstone and honour the memory of Flavinus!" + 
-						"You have {money} for materials."
+						"You have {{sack.money}} for materials."
+		nextNodeUid=   nodeUids.workshopIntro,
 	)
-
-
+    
+    
