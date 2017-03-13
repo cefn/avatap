b6cf873510470c272fd543193142b056cd760867
Cefn Hoile <github.com@cefn.com>
2017-03-13 18:09:53 +0000

    Updated reference example to match new API

--- python/stories/corbridge.py
+++ python/stories/corbridge.py
@@ -1,71 +1,100 @@
-from milecastles import Uid, UidRegistry, Story, Box, LinearPassage
-
+from milecastles import Story, Box, ThroughPage, ThroughSequence, ConditionFork, NodeFork, SackChange
+from engines.console import ConsoleSiteEmulator
 # inspects the module to figure out the story name (e.g. corbridge)
 storyName = __name__.split(".")[-1]
 
 # create story
 story = Story(
     uid=storyName,
-    startNodeUid = "landing",
+    # added for debugging
+    startNodeUid = "stoneFork",
+    #startNodeUid = "landing",
     startSack={
-            money:  100,
-            points: 0,
-            hours:  0,
-            stone:  False,
-            sharp:  False,
+            "money":  100,
+            "points": 0,
+            "hours":  0,
+            "smithAnger": 0,
+            "stone":  False,
+            "sharp":  False,
     }
 )
 
 with story:
     
     # populate with boxes
-    entranceBox =   Box( uid="entrance",    label="Box I",      description="the Market")
-    workshopBox =   Box( uid="workshop",    label="Box II",     description="the Workshop")
-    tombBox =       Box( uid="tomb",        label="Box III",    description="the Tomb")
-    quarryBox =     Box( uid="quarry",      label="Box IV",     description="the Quarry")
+    marketBox =         Box( uid="1",   label="Box I",      description="the Market")
+    workshopBox =       Box( uid="2",   label="Box II",     description="the Workshop")
+    tombBox =           Box( uid="3",   label="Box III",    description="the Tomb")
+    quarryBox =         Box( uid="4",   label="Box IV",     description="the Quarry")
+    
+    entranceBox = marketBox
 
     # populate with passages
-    LinearSequence(
-        uid=            "landing",
-        visitBoxUid =   "entrance",
-        nextNodeUid =   "workshopIntro"
+    ThroughSequence(
+        uid =           "landing",
+        change =        SackChange( 
+                            reset=story.startSack 
+                        ),
+        goalBoxUid =    marketBox.uid,
+        nextNodeUid =   "workshopArrive",
         sequence = [
-            "You are a stone mason working our of the military base at Corbridge.\n" + 
-            "Flavinus, a respected young cavalryman has recently died and members\n" + 
-            "of the Burial Club have approached you to create a tombstone in his honour"
-            ,
-            "Your quest is to gather the materials, tools and inspiration to complete \n" + 
-            "the tombstone and honour the memory of Flavinus!" + 
-            "You have {{sack.money}} for materials."
+            """ You are a stone mason working our of the military base at Corbridge.
+                Flavinus, a respected young cavalryman has recently died and members
+                of the Burial Club have approached you to create a tombstone in his honour""",
+            """ Your quest is to gather the materials, tools and inspiration to complete
+                the tombstone and honour the memory of Flavinus!
+                You have {{sack.money}} for materials.""",
         ],
     )
     
-    ChoicePage(
-        uid=            "workshopIntro",
-        page=           "You are in your workshop. It's early in the morning but you don't have long to complete the tombstone.."
-        choices = {
-                        "sharpening":    "Get your tools sharpened at the blacksmith",
-                        "inspiration":   "Look for inspiration",
-                        "stone":         "Go outside to order stone from the quarry",
-        }
-        otherBoxText=   "Find {{node.visitBox.label}} to go to your workshop",
+    ConditionFork(
+            uid=            "workshopArrive",
+            change =        SackChange( 
+                                plus={"hours":1} 
+                            ),
+            condition =     "sack.sharp or sack.smithAnger >= 2",
+            falseNodeUid =  "workIntro",
+            trueNodeUid =   "ending",
     )
-   
+            
+    ThroughPage(
+        uid=            "workIntro",
+        missPage =      "To begin work go to {{node.goalBox.label}}",
+        page =          "You are in your workshop. It's early in the morning but you don't have long to complete the tombstone.",
+        goalBoxUid =    workshopBox.uid,
+        nextNodeUid =   "work",
+    )
+            
+    NodeFork(
+        uid =   "work",
+        choices = {
+            "sharpening":   "Get your tools sharpened at the blacksmith",
+            "stone":        "Go outside to order stone from the quarry",
+            "inspiration":  "Look for inspiration"
+        },
+        hideChoices = {
+            "sharpening":   "sack.sharp==True",
+            "stone":        "sack.stone==True",
+        },
+    )
+                   
     # ROUTE TO ANGRY OR COMPLIANT BLACKSMITH
     ConditionFork(
         uid=           "sharpening",
-        condition=     "sack.hours > 6 and sack.smithAnger < 2",
+        condition =    "sack.hours > 2 and sack.smithAnger < 3",
         trueNodeUid=   "sharpeningSuccess",
         falseNodeUid=  "sharpeningFailure",
     )
+    
     # compliant blacksmith
-    LinearPage(
-        uid=         "sharpeningSuccess",
+    ThroughPage(
+        uid =           "sharpeningSuccess",
+        goalBoxUid =    marketBox.uid,
         change = SackChange(
-            trigger =   "sack.tool == False",
-            assign =    { tool:True},
-            add  =      { points:1 },
-            remove =    { money:10 },
+            trigger =   "sack.sharp == False",
+            assign =    { "sharp":True},
+            plus  =     { "points":1 },
+            minus =     { "money":10 },
         ),
         page = """  
             {% if node.change.triggered %}
@@ -80,39 +109,85 @@ with story:
                 You already sharpened your tools
             {% endif %}
         """,
-        nextNodeUid = "workshopIntro"
-    )    
+        nextNodeUid = "workshopArrive"
+    )
+        
     #angry blacksmith
-    LinearPage(
-        uid="sharpeningFailure",
-        change =SackChange( 
-                add = { smithAnger:1 } 
+    ThroughPage(
+        uid=            "sharpeningFailure",
+        goalBoxUid=     marketBox.uid,
+        change = SackChange( 
+                plus = { "smithAnger":1 } 
         ),
-        page=   """ {% if sack.smithAnger == 0 %}You walk across the base to the blacksmith's workshop. The blacksmith says 'come back this afternoon, I'm too busy'{% endif %}
-                    {% if sack.smithAnger == 1 %}I said I'm busy!!! Come back later!!{% endif %}
-                    {% if sack.smithAnger == 2 %}That's it, Get out!! Don't come back ever again!{% endif %}
-                    {% if sack.smithAnger >= 3 %}The blacksmith shouts at you: 'I said that you're banned!!' You'll have to work with blunt tools now{% endif %}""", 
-        nextNodeUid = "workshopIntro",
+        page=   """ 
+            {% if sack.smithAnger == 1 %}You walk across the base to the blacksmith's workshop. The blacksmith says 'come back this afternoon, I'm too busy'{% endif %}
+            {% if sack.smithAnger == 2 %}I said I'm busy!!! Come back later!!{% endif %}
+            {% if sack.smithAnger == 3 %}That's it, Get out!! Don't come back ever again!{% endif %}
+            {% if sack.smithAnger >= 4 %}The blacksmith shouts at you: 'I said that you're banned!!' You'll have to work with blunt tools now{% endif %}
+        """, 
+        nextNodeUid = "workshopArrive",
     )
 
     # ROUTE TO INSPIRATION AT TOMB (this has been switched from where it was originally at the workshop)
-    LinearSequence(
+    ThroughSequence(
         uid="inspiration",
-        visitBoxUid = tombBox.uid,
+        goalBoxUid = tombBox.uid,
         sequence = [
             "You've seen cavalrymans' tombstones before but never actually carved one yourself. You'll need to get some ideas. INVESTIGATE the stone carvings in this part of the museum.",
             "Can you find the EAGLE MOUNT that is near this box? Make a sketch if you can. This is a good start but you will need to find more later. Look around until you have enough inspiration"            
-        ]
-        nextNodeUid = "workshopIntro",
+        ],
+        nextNodeUid = "workshopArrive",
     )
     
     # ROUTE TO GET STONE
-    ChoicePageFork(
+    
+    ThroughPage(
         uid="stone",
         page="You are on an East-West road used for supplying the fort. There are a number of shops here.",
+        goalBoxUid = quarryBox.uid,
+        nextNodeUid="stoneFork",
+    )
+    
+    NodeFork(
+        uid="stoneFork",
         choices = {
-                "stoneAlone":    "Carry the stone yourself for a total of {{story.boxes.stoneAlone.change.remove['money']}} denarii",
-                "stoneDonkey":   "Hire a cart to carry it for a total of {{story.boxes.stoneDonkey.change.remove['money']}} denarii",
+                "stoneAlone":    "Pretend to carry the stone yourself back to the workshop for a total of {{choiceNode.change.remove['money']}} denarii",
+                "stoneDonkey":   "Go to {{story.nodes.stoneDonkey.getGoalBox().description}}, pretend to hire and ride a donkey back to the workshop for a total of {{choiceNode.change.remove['money']}} denarii",
         }
     )
     
+    ThroughPage(
+        uid="stoneAlone", 
+        goalBoxUid=workshopBox.uid,
+        change=SackChange(
+            assign={"stone":True},
+            minus={"money":65}
+        ),
+        page="You carried the stone yourself", 
+        nextNodeUid="workshopArrive"
+    )
+
+    ThroughPage(
+        uid="stoneDonkey", 
+        goalBoxUid=marketBox.uid,
+        change=SackChange(
+            assign={"stone":True},
+            minus={"money":65}
+        ),
+        page="You hired a donkey to carry the stone, now ride the donkey back to the workshop", 
+        nextNodeUid="workshopArrive"
+    )
+        
+    ThroughPage(
+        uid="ending",
+        goalBoxUid=entranceBox.uid,
+        page="Respawned. Tap to begin your adventure again",
+        missPage="You completed your adventure with {{sack.points}} points. Return to {{node.goalBox.label}} to respawn.",
+        nextNodeUid = "landing",
+    )
+        
+if __name__ == "__main__":
+    print("Loading emulator")
+    emulator = ConsoleSiteEmulator(story=story)
+    print("Running Emulator")
+    emulator.run()
