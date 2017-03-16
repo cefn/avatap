982b1e36900a6e4b873bb7f2b59d1081ef00c00e
cheapjack <ross@cheapjack.org.uk>
2017-03-16 15:04:56 +0000

    Added stupid

--- python/stories/stupid.py
+++ python/stories/stupid.py
@@ -15,21 +15,24 @@ story = Story(
 )
 
 with story:
-
     homeBox =       Box( uid="1",   label="Box I",      description="at Jack's Mother's house")
     roadBox =       Box( uid="2",   label="Box II",     description="on The Road To Market")
-    
+    beanstalkBox =  Box(uid="1",    label="Box III",    description="beanstalkBox")
+
+# An extra thing to implement some time passing
     incrementTime = SackChange(
         plus={"hours":1}
     )
 
+# A ThroughPage is a node with a uid and you can use operators to
+# interact with the Sack
     ThroughPage(
         uid="landing",
         change=SackChange(
             reset=story.startSack
         ),
         time = incrementTime,
-        page="Jack, you need to sell the cow",
+        page="Jack, you've fallen on hard times, you need to sell the cow",
         goalBoxUid = homeBox.uid,
         nextNodeUid = "sellcow"
     )
@@ -42,17 +45,30 @@ with story:
         ),
         missTemplate="Go to {{node.getGoalBox(story).label}} to sell the cow",
         page="You sold the cow for a handful of beans",
+        goalBoxUid = homeBox.uid,
+        nextNodeUid = "house"
+    )
+
+ThroughPage(
+        uid="plantbeans",
+        time=incrementTime,
+        change = SackChange(
+            assign={"beans":False}
+        ),
+        missTemplate="Go to {{node.getGoalBox(story).label}} to sell the cow",
+        page="You sold the cow for a handful of beans",
         goalBoxUid = roadBox.uid,
         nextNodeUid = "ending"
     )
 
     ThroughPage(
         uid="ending",
-        page="You have finished your adventure. {% if sack.beans %}You have the beans{% else %}You don't have the beans{% endif %}",
+        page="You have finished your adventure.
+        {% if sack.beans %}You have the beans{% else %}You don't have the beans{% endif %}",
         goalBoxUid = homeBox.uid,
         nextNodeUid = "landing"
     )
-    
+
 if __name__ == "__main__":
     print("Loading emulator")
     emulator = ConsoleSiteEmulator(story=story)
