c77ae162a325e5f742a59babbaa596eafba53a9b
Cefn Hoile <github.com@cefn.com>
2017-04-15 00:45:50 +0100

    Added cross compile routines

---
+++ python/cross/cockle/compile.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+COMPILER=../../../micropython/mpy-cross/mpy-cross
+
+find ../ -maxdepth 1 -name '*.py' -exec ${COMPILER} {} \;
+find ../ -maxdepth 1 -name '*.mpy' -exec mv {} ./ \;
+
+find ../stories/ -maxdepth 1 -name '*.py' -exec ${COMPILER} {} \;
+find ../stories/ -maxdepth 1 -name '*.mpy' -exec mv {} ./ \;
