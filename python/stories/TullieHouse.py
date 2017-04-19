be65f57a27d3c2117cc7129f39549d621c135340
cheapjack <ross@cheapjack.org.uk>
2017-04-19 23:47:23 +0100

    Fixed some typos in gkboulter awesome game

--- python/stories/TullieHouse.py
+++ python/stories/TullieHouse.py
@@ -19,225 +19,232 @@ story = Story(
 )
 
 with story:
-    
+
     # populate with boxes
-    
+
     fortBox =         Box( uid="1",   label="Box I",      description="the Fort")
     gateBox =       Box( uid="2",   label="Box II",     description="the Arena Gates")
     arenaBox =           Box( uid="3",   label="Box III",    description="the Arena")
     garrisonBox =         Box( uid="4",   label="Box IV",     description="the Garrison")
-    
+
     entranceBox = fortBox
-    
+
 	#count     123456789x123456789x123456
-	
+
 	#1. Intro / Prep
-	
+
     ThroughSequence(
 		uid =           "landing",
-		change =        SackChange( 
-							reset=story.startSack 
+		change =        SackChange(
+							reset=story.startSack
 							),
 		goalBoxUid =    fortBox.uid,
 		nextNodeUid =   "mask1",
 		sequence = [
-            """ Welcome to Milecastles! 
-				Look for numbered boxes 
-				and tap your card to  
+            """ Welcome to Milecastles!
+				Look for numbered boxes
+				and tap your card to
 				progress the story!""",
-            """ You are Grecus 
-				Turmaerious, a member of 
-				the glorious Super Turma 
-				Cavalry Troop!
-				Skilled on horseback and
+            """ You are Grecus
+				Turmaerious, a member of
+				the glorious Super Turma
+				Cavalry Troop!""",
+            """ Skilled on horseback and
 				favoured by the God of
 				Epona, you could say that
-				you're a super trooper!""",	
+				you're a super trooper!""",
             """ Your quest is to show
 				off your riding, archery
-				and sword skills in front
-				of the assmebled crowd
-				at the Hippika Gymnasia 
-				game season at Uxelodunum 
-				fort.""",
-			""" Go over to the fort 
+				and sword skills at the
+                Hippika Gymnasia games
+                at Uxelodunum fort.""",
+			""" Go over to the fort
 				entrance to start your
 				quest!""",
         ],
     )
 	#count     123456789x123456789x123456
-	
+
     ThroughSequence(
 		uid =           "mask1",
-		change =        SackChange( 
-							reset=story.startSack 
+		change =        SackChange(
+							reset=story.startSack
 							),
 		goalBoxUid =    gateBox.uid,
 		nextNodeUid =   "maskFork1",
 		sequence = [
             """ You are at the entrance
 				to the games arena.
-				Before entering, you need 
+				Before entering, you need
 				to choose your team.
 				Will you be:""",
             """ A Trojan with a
 				focus on team work
 				and sword skills?
-				or...""",	
+				or...""",
             """ ...an Amazon, with
 				a focus on archery
-				and horse riding 
+				and horse riding
 				skills?""",
         ],
     )
-	
+
     NodeFork(
         uid =   "maskFork1",
         choices = {
-			"trojan1":    	""" Collect the Trojan
-								mask from the fort. """,
-            "amazon1":   	""" Collect the Amazon
-								mask from the garrison.""",
-				}
-			)
-	
+			"trojan1": """
+            Collect the Trojan
+            mask from the fort.
+            """,
+            "amazon1": """
+            Collect the Amazon
+            mask from the garrison.
+            """,
+            }
+        )
+
 	#2 Amazon / Trojan selection
-				
 				#count     123456789x123456789x123456
-	
+
     ThroughPage(
 		uid="trojan1",
-		missTemplate =  """ This isn't the fort! 
+		missTemplate =  """ This isn't the fort!
 							Go to {{node.goalBox.label}}""",
-        page=			""" You search the armoury 
-							at the fort and find a 
-							Trojan mask. 
-							TAKE A SELFIE wearing a 
-							helmet in the SELFIE 
+        page=			""" You search the armoury
+							at the fort and find a
+							Trojan mask.
+							TAKE A SELFIE wearing a
+							helmet in the SELFIE
 							AREA!""",
 		goalBoxUid = fortBox.uid,
 		nextNodeUid="trojan2",
     )
-    
+
     ThroughPage(
 		uid="trojan2",
-		change =        SackChange( 
+		change =        SackChange(
 							assign={"trojan":True},
 							plus={"sword":10}
 							),
-		missTemplate =  """ This isn't the fort! 
+		missTemplate =  """ This isn't the fort!
 							Go to {{node.goalBox.label}}""",
-        page=			""" You chose Trojan! 
-							Your sword skills went up 
-							by{{node.change.plus['sword']}}!
-							Now return to your Turma at 
+        page=			""" You chose Trojan!
+							Your sword skills went up
+							by {{node.change.plus['sword']}}!
+							Now return to your Turma at
 							the arena gates.""",
 		goalBoxUid = fortBox.uid,
 		nextNodeUid="gate1",
     )
-    
+
     #3 amazon section
 				#count     123456789x123456789x123456
-	
+
     ThroughPage(
 		uid="amazon1",
-		missTemplate =  """ This isn't the garrison! 
+		missTemplate =  """ This isn't the garrison!
 							Go to {{node.goalBox.label}}""",
-        page=			""" You search the garrison 
-							and find the Amazon mask. 
-							TAKE A SELFIE wearing a 
-							helmet in 
-							the SELFIE AREA!""",
+        page=			""" You search the garrison
+							and find the Amazon mask.
+							TAKE A SELFIE wearing a
+							helmet in the
+                            SELFIE AREA!""",
 		goalBoxUid = garrisonBox.uid,
 		nextNodeUid="amazon2",
     )
 
     ThroughPage(
 		uid="amazon2",
-		change =        SackChange( 
+		change =        SackChange(
 							assign={"amazon":True},
 							plus={"archery":10}
 							),
-		missTemplate =  """ This isn't the fort! 
+		missTemplate =  """ This isn't the fort!
 							Go to {{node.goalBox.label}}""",
         page=			""" You chose Trojan! Your
 							archery skills went up
 							by {{node.change.plus['archery']}}!
-							Now return to your Turma 
+							Now return to your Turma
 							at the arena gates.""",
 		goalBoxUid = garrisonBox.uid,
 		nextNodeUid="gate1",
     )
- 
+
 	#count     123456789x123456789x123456
-		
+
     ThroughSequence(
 		uid =           "gate1",
 		goalBoxUid =    gateBox.uid,
 		nextNodeUid =   "trainingFork1",
 		sequence = [
-            """ You return to the 
+            """ You return to the
 				gates and meet with
 				members of your Turma.
 				They are talking about
 				a strategy tablet
 				called the Ars Technica.""",
             """ You think about going
-				to collect it from 
+				to collect it from
 				Victor, a groom visiting
 				from Arbeia, but you
 				still need to prepare
-				for the games.""",	
+				for the games.""",
             """ You only have time to
 				do so much before the
-				games begin! Choose 
+				games begin! Choose
 				from the following
 				options:""",
         ],
     )
-	
+
 	#4 training fork
-	
+
     NodeFork(
         uid =   "trainingFork1",
         choices = {
-			"arsTech1":    	""" Collect the tablet
-								from Victor at the fort. """,
-            "epona1":   	""" Make an offering to
-								Epona at the garrison.""",
-			"weapon1":   	""" Train with your main
-								weapon at the arena.""",
-				}
+            "arsTech1": """
+            Collect the tablet
+            from Victor at the fort.
+            """,
+            "epona1": """
+            Make an offering to
+            Epona at the garrison.
+            """,
+            "weapon1": """
+            Train with your main
+            weapon at the arena.
+            """,
+            }
 			)
-			
+
 				#count     123456789x123456789x123456
-	
+
     ThroughPage(
 		uid="arsTech1",
-		change =        SackChange( 
+		change =        SackChange(
 							assign={"arstech":True},
 							),
-		missTemplate =  """ This isn't the fort! 
+		missTemplate =  """ This isn't the fort!
 							Go to {{node.goalBox.label}}""",
         page=			""" You track down Victor
 							and he lets you read
 							his Ars Technica tablet.
 							INVESTIGATE the displays
-							for info on tactics. This 
+							for info on tactics. This
 							will be useful later...""",
 		goalBoxUid = fortBox.uid,
 		nextNodeUid="rest1",
     )
-	
+
     ThroughPage(
 		uid="epona1",
-		change =        SackChange( 
+		change =        SackChange(
 							plus={"horse":20},
 							),
-		missTemplate =  """ This isn't the garrison! 
+		missTemplate =  """ This isn't the garrison!
 							Go to {{node.goalBox.label}}""",
         page=			""" You return to your
-							garrison and find the 
+							garrison and find the
 							altar you brought with
 							you from Senhouse.
 							Make an offfering of
@@ -246,8 +253,8 @@ with story:
 		goalBoxUid = garrisonBox.uid,
 		nextNodeUid="rest1",
     )
-    
-    
+
+
     ConditionFork(
         uid=           "weapon1",
         condition =    "sack.archery >= 20",
@@ -255,13 +262,13 @@ with story:
         falseNodeUid=  "sword1",
     )
 
-	
+
     ThroughPage(
 		uid="archery1",
-		change =        SackChange( 
+		change =        SackChange(
 							plus={"archery":20},
 							),
-		missTemplate =  """ This isn't the arena! 
+		missTemplate =  """ This isn't the arena!
 							Go to {{node.goalBox.label}}""",
         page=			""" The arena is quiet
 							before the games.
@@ -272,32 +279,32 @@ with story:
 		goalBoxUid = arenaBox.uid,
 		nextNodeUid="rest1",
     )
-	
+
     ThroughPage(
 		uid="sword1",
-		change =        SackChange( 
+		change =        SackChange(
 							plus={"sword":20},
 							),
-		missTemplate =  """ This isn't the arena! 
+		missTemplate =  """ This isn't the arena!
 							Go to {{node.goalBox.label}}""",
         page=			""" The arena is quiet
 							before the games.
 							You take the time to
 							PRACTICE your sword
-							technique, raising 
+							technique, raising
 							your sklll to {{node.change.plus['archery']}}.""",
 		goalBoxUid = arenaBox.uid,
 		nextNodeUid="rest1",
     )
-	
-	
+
+
 	# variables check (take out later)
-	
+
     ThroughSequence(
 		uid =           "rest1",
 		goalBoxUid =    fortBox.uid,
 		nextNodeUid =   "landing",
-		sequence = [
+        sequence = [
             """ You take a short
 				rest after your
 				training. You think
@@ -308,7 +315,7 @@ with story:
 				Ars Technica? {{sack.arstech}}.
 				Horse skill level? {{sack.horse}}.
 				Sword Skill level? {{sack.sword}}.
-				Archery skill level? {{sack.archery}}""",	
+				Archery skill level? {{sack.archery}}""",
             """ You give your horse
 				a ride around the training
 				circuit to give him
@@ -317,10 +324,10 @@ with story:
 				Ready?""",
         ],
     )
-						    
+
 if __name__ == "__main__":
     print("Loading emulator")
     emulator = ConsoleSiteEmulator(story=story)
     print("Running Emulator")
     emulator.run()
-    
+
