327516316c9e4a4d6f26ac88d0ef549691dae637
Cefn Hoile <github.com@cefn.com>
2017-04-19 08:17:39 +0100

    Changed to use str#format() to minimise heap fragmentation. Fixed implicit bug where card and story could be created as tuple by trailing comma. Separated out stages of template compilation to allow future substitution of cached render functions. Introduced garbage collection routines in template compilation routine.

--- python/engine.py
+++ python/engine.py
@@ -1,14 +1,13 @@
-from agnostic import io
+from agnostic import io,gc
 from milecastles import AnonymousContainer, Holder, Story, signature, required, optional, debug
 from boilerplate import Compiler, Resolver
 
-# TODO space added after args as workaround for https://github.com/pfalcon/utemplate/issues/5
-templatePrefix = "{% args " + signature + " %} "
+templatePrefix = "{{% args {} %}}".format(signature)
 
 class Engine(AnonymousContainer):
     box = required
-    card = None,
-    story = None,
+    card = None
+    story = None
     node = None
 
     def __init__(self, *a, **k):
@@ -59,40 +58,58 @@ class Engine(AnonymousContainer):
     
     def evaluateExpression(self, expression):
         return eval(expression, self.getEngineContext())
-   
-    def fillNodeTemplate(self, node, templateString):
+
+    def compileGeneratorFactory(self, node, templateString):
         # prefix the templateString with the standard argument signature
         templateString = templatePrefix + templateString
-        # use node as its own resolver
+        # use node as its own resolver (named templates referenced should be attributes of node)
         templateResolver = Resolver(node)
         # create streams and wire them
         template_in = io.StringIO(templateString)
         template_out = io.StringIO()
         c = Compiler(template_in, template_out, loader=templateResolver)
+        # do a compile run
         try:
-            # do a compile run
             c.compile()
-            compiled = template_out.getvalue()
+            gc.collect()
+            return template_out.getvalue()
+        except Exception as k:
+            raise k
+
+    def loadGeneratorFactory(self, node, templateString):
+        try:
+            compiled = self.compileGeneratorFactory(node, templateString)
+            # TODO CH MEMORY 'Cache' pre-compiled modules, indexed by MD5 hash of templateString (use import from filesystem/frozen module not exec from memory)
             g = dict()
             # evaluate the compiled code
             exec(compiled, g)
             # extract the render function which was created during exec
-            renderFun = g["render"]
-            # create a string generator by calling the render function, passing context arguments
-            renderGen = renderFun(**self.getEngineContext())
-            # concatenate the generated strings
-            renderOut = io.StringIO()
-            for chunk in renderGen:
-                renderOut.write(chunk)
-            # return concatenated string
-            renderedString = renderOut.getvalue() 
-            # TODO preceding space removed here as workaround for https://github.com/pfalcon/utemplate/issues/5
-            return renderedString[1:] 
+            gc.collect()
+            return g["render"]
         except Exception as k:
             raise k
-    
+
+    def constructGenerator(self, node, templateString):
+        renderFun = self.loadGeneratorFactory(node, templateString)
+        # create a string generator by calling the render function, passing context arguments
+        generator = renderFun(**self.getEngineContext())
+        gc.collect()
+        return generator
+
+    # TODO CH MEMORY - Avoid fragmentation from concatenating strings - pass generator directly to newly improved bitfont (which accepts generators)
+    def concatenateGeneratedStrings(self, node, templateString):
+        renderGen = self.constructGenerator(node, templateString)
+        # concatenate the generated strings
+        renderOut = io.StringIO()
+        for chunk in renderGen:
+            renderOut.write(chunk)
+        # return concatenated string
+        renderedString = renderOut.getvalue()
+        gc.collect()
+        return renderedString
+
     def displayNode(self, node):
-        nodeText = self.fillNodeTemplate(node, node.render(self))
+        nodeText = self.concatenateGeneratedStrings(node, node.render(self))
         if debug:
             debug.debug("SACK" + str(self.card.sack))
         self.displayText(nodeText)
