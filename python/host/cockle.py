aa185edddbba44f7d4405a4dcb1c814992663f80
Cefn Hoile <github.com@cefn.com>
2017-06-07 11:50:36 +0100

    Reimplemented cockle host to minimise stack variables (only host remains)

--- python/host/cockle.py
+++ python/host/cockle.py
@@ -2,9 +2,14 @@ import loader
 from machine import SPI, Pin
 from mfrc522 import MFRC522
 from st7920 import Screen
+from faces.font_5x7 import font as smallFont
+from faces.font_ncenB18 import font as bigFont
 from vault import BankVault
-from engines import dictToCard, cardToDict
+from engines import Engine, dictToCard, cardToDict
 from host import Host
+from milecastles import Box
+
+from stories.corbridge import story
 
 class CockleRfid(BankVault):
 
@@ -16,19 +21,22 @@ class CockleRfid(BankVault):
     def writeCard(self, card, unselect=True):
         return self.writeJson( cardToDict(card), card.uid, unselect)
 
-def launch(story, boxUid):
+def prepareHost(story, boxUid):
     # prepare elements
     spi = SPI(1, baudrate=1800000, polarity=0, phase=0)
     spi.init()
     screen = Screen(spi=spi, slaveSelectPin=Pin(15))
     blackPlotter = screen.create_plotter(False)
     whitePlotter = screen.create_plotter(True)
-    rfid = CockleRfid(MFRC522(spi=spi, gpioRst=0, gpioCs=2))
+    reader = MFRC522(spi=spi, gpioRst=0, gpioCs=2)
+    rfid = CockleRfid(reader)
+    box = story._get_table(Box)[boxUid]
+    engine = Engine(box=box)
+    engine.registerStory(story)
 
-    # configure host which will perform UI cycle
-    host = Host(
+    return Host(
         story=story,
-        box=boxTable[boxUid],
+        box=box,
         engine=engine,
         screen=screen,
         rfid=rfid,
@@ -38,8 +46,10 @@ def launch(story, boxUid):
         whitePlotter=whitePlotter,
     )
 
-    # Consider converging on Runnable factory instead
+if __name__ == "__main__":
+    print("Running Main")
+    # configure host
+    host = prepareHost(story, "1")
+    # perform UI cycle
     while host.running:
         host.gameLoop()
-
-
