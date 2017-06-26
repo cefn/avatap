4c3514ed24675881aebc7ef47e6bfb3f7a0fdd0d
Cefn Hoile <github.com@cefn.com>
2017-06-26 22:34:07 +0100

    Removed now-spurious commented historical configuration

--- python/regimes/integration_test.py
+++ python/regimes/integration_test.py
@@ -12,38 +12,12 @@ readerSpi = SPI(1, baudrate=1800000, polarity=0, phase=0)
 readerSpi.init()
 agnostic.collect()
 
-"""
-screenMiso = Pin(12, Pin.IN)
-screenMosi = Pin(13, Pin.OUT)
-screenSck = Pin(14, Pin.OUT)
-"""
-
-"""
-Screen SoftwareSPI( miso D0/GPIO16 mosi D1/GPIO5 sck D2/GPIO4 ), Reader HardwareSPI: Both functional
-"""
-
-#screenSpi = readerSpi
-
-# miso unused but has to be allocated
-#screenMiso = Pin(1) # main UART TX pin - works - kills Serial
-#screenMiso = Pin(3) # main UART RX pin - works - kills Serial
-#screenMiso = Pin(12) # same as hardware SPI - doesn't work
-
-"""
-screenMiso = Pin(16) # D0 works, but it's the only spare pin
-screenMosi = Pin(5)
-screenSck = Pin(4)
-screenSpi = SPI(-1, baudrate=1800000, polarity=0, phase=0, miso=screenMiso, mosi=screenMosi, sck=screenSck)
-agnostic.collect()
-"""
-
 screenMiso = Pin(16)
 screenMosi = Pin(5)
 screenSck = Pin(4)
 screenSpi = SPI(-1, baudrate=1800000, polarity=0, phase=0, miso=screenMiso, mosi=screenMosi, sck=screenSck)
 agnostic.collect()
 
-#rdr = MFRC522(spi=readerSpi, gpioRst=0, gpioCs=2) # equivalent to reset D3 (was CableSelect D4)
 rdr = MFRC522(spi=readerSpi, gpioRst=None, gpioCs=2) # equivalent to reset D3 (was CableSelect D4)
 agnostic.collect()
 
