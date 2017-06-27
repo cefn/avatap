445142ed8a634f9ed91f65cc76fbe15c30a8b988
Cefn Hoile <github.com@cefn.com>
2017-06-27 13:32:08 +0100

    Added power pin initialisation, fixed label rectangle calculation, made generator-renderer also use minimal draw area (not wipe whole screen). Introduced card value passed into gameLoop allowing fuzzing of host logic alongside other engines. Removed unnecessary wipe, bringing 'saving' message forward in the sequence. Changed default behaviour of readCard call to leave card select in place. Added full screen redraw workaround for label whose draw location is mysteriously misplaced. Implemented card return value to help indicate idleness to caller (where no card was presented). Implemented default second-long high-pulse for powerdown.

--- python/host/__init__.py
+++ python/host/__init__.py
@@ -16,13 +16,14 @@ class Host(AnonymousContainer):
     bigFont = required
     blackPlotter = required
     whitePlotter = required
+    powerPin = required
     running = True
     expectStay = False
 
     def displayGeneratedText(self, generator):
         self.screen.clear()
-        self.smallFont.draw_generator(generator, self.blackPlotter)
-        self.redraw()
+        (x,y) = self.smallFont.draw_generator(generator, self.blackPlotter)
+        self.redraw((0,0,x,y))
 
     def toast(self, para, redraw=True):
         x = 0
@@ -36,11 +37,11 @@ class Host(AnonymousContainer):
     def label(self, line, redraw=True):
         labelWidth = self.smallFont.line_cols(line)
         labelHeight = self.smallFont.height
-        left = self.screen.width - labelWidth - 1
-        top = self.screen.height - labelHeight - 1
-        right = left + labelWidth + 2
-        bottom = top + labelHeight + 2
-        self.screen.fill_rect(left - 2, top - 2, right, bottom, set=True) # color box in black
+        right = self.screen.width
+        bottom = self.screen.height
+        left = right - (labelWidth + 2)
+        top = bottom - (labelHeight + 2)
+        self.screen.fill_rect(left, top, right, bottom, set=True) # color box in black
         self.smallFont.draw_line(line, x=left + 1, y=top + 1, plotter=self.whitePlotter)
         rect = (left, top, right, bottom)
         if redraw:
@@ -59,13 +60,14 @@ class Host(AnonymousContainer):
             self.screen.redraw()
 
     # TODO CH add 'special control tag' handling behaviour, based around host 'expectWipeNext'
-    def gameLoop(self):
+    # TODO either fully remove or fully implement timeout
+    def gameLoop(self, card=None):
         loopStart = agnostic.ticks_ms()
         print("START {}".format(loopStart))
         toastRect = None
         labelRect = None
         if self.expectStay:
-            labelRect = self.label(awaitReplace) # draw rapidly before full screen refresh
+            labelRect = self.label(awaitReplace) # draw quickly before clearing rest of screen
             self.screen.clear()
             labelRect = self.label(awaitReplace, redraw=False)
         else:
@@ -73,9 +75,13 @@ class Host(AnonymousContainer):
             toastRect = self.toast(b"PLACE TAG\nto read", redraw=False)
         self.redraw()
         try:
-            cardUid = None
-            while cardUid is None: # hang here until a cardUid is seen
-                print("presencing ({})".format(agnostic.ticks_ms() - loopStart)); taskStart = agnostic.ticks_ms()
+            if card is not None: # handle 'simulated' card passed in as argument
+                cardUid = card.uid
+            else:
+                cardUid = None
+
+            while cardUid is None: # remain here until a cardUid is seen or timeout is hit
+                print("presenting ({})".format(agnostic.ticks_ms() - loopStart)); taskStart = agnostic.ticks_ms()
                 cardUid = self.rfid.awaitPresence()
                 print("present! +{}".format(agnostic.ticks_ms() - taskStart))
         finally:
@@ -84,19 +90,19 @@ class Host(AnonymousContainer):
             if labelRect:
                 self.wipeRect(labelRect)
         labelRect = self.label(b"KEEP IN PLACE, loading..")
-        try:
-            print("loading ({})".format(agnostic.ticks_ms() - loopStart)); taskStart = agnostic.ticks_ms()
-            card = self.rfid.readCard(cardUid=cardUid)
-            print("loaded! +{}".format(agnostic.ticks_ms() - taskStart))
-            if card is None or not(card.storyUid == self.story.uid):
-                card = self.story.createBlankCard(cardUid)
-        finally:
-            self.wipeRect(labelRect)
+        print("loading ({})".format(agnostic.ticks_ms() - loopStart)); taskStart = agnostic.ticks_ms()
+        if card is None:
+            # TODO need to detect read failure - INVALID card content means reset to blank but failed read should go back to "Place Tag"
+            card = self.rfid.readCard(cardUid=cardUid, unselect=False) # expecting to remain selected for write
+        print("loaded! +{}".format(agnostic.ticks_ms() - taskStart))
+        if card is None or not(card.storyUid == self.story.uid):
+            card = self.story.createBlankCard(cardUid)
+        labelRect = self.label(b"KEEP IN PLACE, saving...") # TODO CH bring this until before text is shown, so people don't forget
         origNodeUid = card.nodeUid
         print("handling ({})".format(agnostic.ticks_ms() - loopStart));taskStart = agnostic.ticks_ms()
         nextNode = self.engine.handleCard(card, self)
         print("handled! +{}".format(agnostic.ticks_ms() - taskStart))
-        # TODO CH accelerate case where card has not been changed (e.g. wrong box)
+        # TODO CH accelerate case where nodeUid not changed (e.g. wrong box - no need to rewrite)
         # will next page also be at this box?
         if issubclass(type(nextNode), GoalPage) and nextNode.goalBoxUid == self.box.uid:
             self.expectStay = True # goalpage at same box
@@ -104,31 +110,23 @@ class Host(AnonymousContainer):
             self.expectStay = True # remote GoalPage or NodeFork unvisited
         else:
             self.expectStay = False # remote GoalPage or NodeFork now visited
-        labelRect = self.label(b"KEEP IN PLACE, saving...")
         try:
             print("saving ({})".format(agnostic.ticks_ms() - loopStart));taskStart = agnostic.ticks_ms()
-            self.rfid.writeCard(card)
+            self.rfid.writeCard(card, unselect=True) # unselecting, expecting card to be taken away
             print("saved! +{}".format(agnostic.ticks_ms() - taskStart))
         finally:
             self.wipeRect(labelRect)
-        labelRect = self.label(awaitLift if self.expectStay else awaitLeave)
+        labelRect = self.label(awaitLift if self.expectStay else awaitLeave, redraw=False)
+        self.redraw() # TODO workaround for draw alignment bug which seems only to affect this message
         try:
             print("removing ({})".format(agnostic.ticks_ms() - loopStart));taskStart = agnostic.ticks_ms()
             self.rfid.awaitAbsence()
             print("removed! {}".format(agnostic.ticks_ms() - taskStart))
         finally:
             self.wipeRect(labelRect)
+        return card
 
     def powerDown(self):
-        pass # needs implementing
-
-    def createRunnable(self):
-        def run():
-            while self.running:
-                try:
-                    self.gameLoop()
-                except Exception as e:
-                    print(type(e).__name__ + str(e))
-                    if type(e) is not AssertionError:
-                        raise e
-        return run
+        self.powerPin.value(1) # wired to OFF on Polulu Power Switch LV -
+        sleep(1) # only seen in testing with LED attached - otherwise the pulse above should power off the unit
+        self.powerPin.value(0)
