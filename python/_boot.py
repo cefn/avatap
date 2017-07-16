684ed62c1bb400d876beb56708d5f6200841dc53
Cefn Hoile <github.com@cefn.com>
2017-07-16 17:19:55 +0100

    Added fuzzing logic.

--- python/_boot.py
+++ python/_boot.py
@@ -18,25 +18,79 @@ except OSError:
 gc.collect()
 
 import agnostic
-from host.cockle import prepareHost
+from host.cockle import prepareHosts
 import loader
+from os import urandom
+from machine import reset
 agnostic.collect()
+
+
+def floor(n):
+    res = int(n)
+    return res if res == n or n >= 0 else res-1
+
+def log2approx(val):
+    val = floor(val)
+    approx = 0
+    while val != 0:
+        val &= ~ (1 << approx)
+        approx = approx + 1
+    return approx
+
+def randint(minVal, maxVal=None):
+    if (maxVal != None):
+        return minVal + randint(maxVal - minVal)
+    else:
+        maxVal = minVal
+    byteCount = (log2approx(maxVal) // 8) + 1  # each byte is 8 powers of two
+    val = 0
+    randBytes = urandom(byteCount)
+    idx = 0
+    while idx < len(randBytes):
+        val |= randBytes[idx] << (idx * 8)
+        idx += 1
+    del randBytes
+    return val % maxVal
+
 
 story = loader.loadStory(loader.storyUid)
 agnostic.collect()
 
-boxHosts = prepareHost(story, [loader.boxUid])
+from milecastles import Box
 
-boxHost = boxHosts[loader.boxUid]
-while True: # useful for testing powerDown, although loop runs only once if Polulu latching
-    while boxHost.running:
+def fuzz():
+    """Emulate whole museum in one box"""
+
+    boxUids = list(story._get_table(Box).keys())
+    boxHosts = prepareHosts(story, boxUids)
+
+    while True:
+        #agnostic.reboot_collect()
         agnostic.report_collect()
-        try:
-            cardUid = boxHost.newLoop() # card value allows gameLoop to indicate idleness for future self-depower
-        except Exception as e:
-            sys.print_exception(e)
-    print("Powering down")
-    boxHost.powerDown()
+        nextBoxUid = boxUids[randint(len(boxUids))]
+        print("Visiting box {}".format(nextBoxUid))
+        nextHost = boxHosts[nextBoxUid]
+        nextHost.cardCache = None
+        cardUid = nextHost.gameLoop(fuzz=True)
+
+def run():
+    boxHosts = prepareHosts(story, [loader.boxUid])
+    boxHost = boxHosts[loader.boxUid]
+    while True:  # useful for testing powerDown, although loop runs only once if Polulu latching
+        while boxHost.running:
+            #agnostic.reboot_collect()
+            agnostic.report_collect()
+            try:
+                cardUid = boxHost.gameLoop()  # card value allows gameLoop to indicate idleness for future self-depower
+            except Exception as e:
+                sys.print_exception(e)
+        print("Powering down")
+        boxHost.powerDown()
+
+try:
+    fuzz()
+except MemoryError:
+    reset()
 
 """
 from regimes.integration_test import run
