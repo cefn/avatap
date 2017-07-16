49d4e67bfe177344e49fda8bfac7b2dc343c30e6
Cefn Hoile <github.com@cefn.com>
2017-07-16 17:26:15 +0100

    Added strategic garbage collection

--- python/loader.py
+++ python/loader.py
@@ -1,16 +1,18 @@
 #Attempts to import everything in the proper sequence
 # garbage collecting between and reporting remaining memory
-from agnostic import report_import
+from agnostic import report_import, report_collect
 
-storyUid = "corbridge"
+storyUid = "arbeia"
 boxUid = "1"
 
 def loadDisplay():
+    report_collect()
     report_import("machine")
     report_import("st7920")
     report_import("faces.font_5x7")
 
 def loadOther():
+    report_collect()
     #report_import("faces.font_timB14")
     report_import("mfrc522")
     report_import("milecastles")
@@ -20,6 +22,7 @@ def loadOther():
     report_import("vault")
 
 def loadStory(name):
+    report_collect()
     imp = report_import("stories." + name)
     mod = getattr(imp, name)
     return getattr(mod, 'story')
